fc666b40ebd05fe9ea41794d584127b4e2fcc9c5
僕は神だ <ajmalshahabudeen@gmail.com>
2025-05-16 14:38:47 +0530

    color done

--- components/qr.tsx
+++ components/qr.tsx
@@ -4,12 +4,40 @@ import React, { useEffect, useRef, ChangeEvent } from "react"
 import { RxDownload } from "react-icons/rx"
 import { Extension, useOptionsStore } from "@/store/useOptionsStore"
 import { useQRCodeStore } from "@/store/useQRCodeStore"
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select"
+import { Button } from "@/components/ui/button"
+import { Input } from "./ui/input"
+import { ColorPicker, IColor, useColor } from "react-color-palette"
+import "react-color-palette/css"
+import {
+	Sheet,
+	SheetContent,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from "./ui/sheet"
 
 // Define Extension type explicitly since it's not exported from the package
 
 export default function QRC() {
 	const ref = useRef<HTMLDivElement>(null)
-	const { options, setOptions, fileExt, setFileExt } = useOptionsStore()
+	const {
+		options,
+		setOptions,
+		fileExt,
+		setFileExt,
+		setBackgroundColor,
+		setDotsColor,
+		setCornersColor,
+		setCornersDotColor,
+		resetColors,
+	} = useOptionsStore()
 	const { qrCode, initializeQRCode } = useQRCodeStore()
 
 	// Initialize the QR code instance once
@@ -36,8 +64,8 @@ export default function QRC() {
 		setOptions({ data: event.target.value })
 	}
 
-	const onExtensionChange = (event: ChangeEvent<HTMLSelectElement>) => {
-		setFileExt(event.target.value as Extension)
+	const onExtensionChange = (valve: string) => {
+		setFileExt(valve as Extension)
 	}
 
 	const onDownloadClick = () => {
@@ -46,31 +74,154 @@ export default function QRC() {
 		}
 	}
 
+	//color logic
+	const [colorBg, setColorBg] = useColor("#ffffff")
+	const [colorDot, setColorDot] = useColor("#ffffff")
+	const [colorCorner, setColorCorner] = useColor("#ffffff")
+	const [colorCornerDot, setColorCornerDot] = useColor("#ffffff")
+
+	const handleBg = (color: IColor) => setBackgroundColor(color.hex)
+	const handleDot = (color: IColor) => setDotsColor(color.hex)
+	const handleCorner = (color: IColor) => setCornersColor(color.hex)
+	const handleCornerDot = (color: IColor) => setCornersDotColor(color.hex)
+
 	return (
 		<div className='flex flex-col'>
 			<h2>Your QR Code</h2>
 			<hr className='my-5' />
 			<div ref={ref}>Loading</div>
 			<div className='flex flex-col gap-5 pt-5'>
-				<input
+				<Input
 					value={options.data}
 					onChange={onDataChange}
-					className='border p-2 rounded-md'
+					placeholder='Website, text, etc'
 				/>
-				<select
-					onChange={onExtensionChange}
-					value={fileExt}
-					className='border p-2 rounded-md'>
-					<option value='svg'>SVG</option>
-					<option value='png'>PNG</option>
-					<option value='jpeg'>JPEG</option>
-					<option value='webp'>WEBP</option>
-				</select>
-				<button
-					onClick={onDownloadClick}
-					className='border p-2 rounded-md inline-flex items-center gap-2 place-content-center'>
-					Download <RxDownload />
-				</button>
+				<div className='flex flex-col md:flex-row justify-center items-center w-full gap-5'>
+					<Sheet>
+						<SheetTrigger asChild>
+							<Button
+								variant={"outline"}
+								className='w-full md:w-[240px]'>
+								Change Background Color
+							</Button>
+						</SheetTrigger>
+						<SheetContent
+							side='bottom'
+							className='w-full md:w-[400px] place-self-center rounded-t-2xl'>
+							<SheetHeader>
+								<SheetTitle className='text-center '>
+									You are now changing background
+								</SheetTitle>
+								<hr className='my-5 bg-red-500' />
+								{/* <SheetDescription> */}
+								<ColorPicker
+									color={colorBg}
+									onChange={setColorBg}
+									onChangeComplete={handleBg}
+								/>
+								{/* </SheetDescription> */}
+							</SheetHeader>
+						</SheetContent>
+					</Sheet>
+					<Sheet>
+						<SheetTrigger asChild>
+							<Button
+								variant={"outline"}
+								className='w-full md:w-[240px]'>
+								Change Dots Color
+							</Button>
+						</SheetTrigger>
+						<SheetContent
+							side='bottom'
+							className='w-full md:w-[400px] place-self-center rounded-t-2xl'>
+							<SheetHeader>
+								<SheetTitle className='text-center '>
+									You are now changing dots
+								</SheetTitle>
+								<hr className='my-5 bg-red-500' />
+								<ColorPicker
+									color={colorDot}
+									onChange={setColorDot}
+									onChangeComplete={handleDot}
+								/>
+							</SheetHeader>
+						</SheetContent>
+					</Sheet>
+					<Sheet>
+						<SheetTrigger asChild>
+							<Button
+								variant={"outline"}
+								className='w-full md:w-[240px]'>
+								Change Corners Color
+							</Button>
+						</SheetTrigger>
+						<SheetContent
+							side='bottom'
+							className='w-full md:w-[400px] place-self-center rounded-t-2xl'>
+							<SheetHeader>
+								<SheetTitle className='text-center '>
+									You are now changing corners
+								</SheetTitle>
+								<hr className='my-5 bg-red-500' />
+								<ColorPicker
+									color={colorCorner}
+									onChange={setColorCorner}
+									onChangeComplete={handleCorner}
+								/>
+							</SheetHeader>
+						</SheetContent>
+					</Sheet>
+					<Sheet>
+						<SheetTrigger asChild>
+							<Button
+								variant={"outline"}
+								className='w-full md:w-[240px]'>
+								Change Corners Dot Color
+							</Button>
+						</SheetTrigger>
+						<SheetContent
+							side='bottom'
+							className='w-full md:w-[400px] place-self-center rounded-t-2xl'>
+							<SheetHeader>
+								<SheetTitle className='text-center '>
+									You are now changing corners dot
+								</SheetTitle>
+								<hr className='my-5 bg-red-500' />
+								<ColorPicker
+									color={colorCornerDot}
+									onChange={setColorCornerDot}
+									onChangeComplete={handleCornerDot}
+								/>
+							</SheetHeader>
+						</SheetContent>
+					</Sheet>
+					<Button
+						variant={"outline"}
+						className='w-full md:w-[240px]'
+						onClick={resetColors}>
+						Reset Colors
+					</Button>
+				</div>
+				<div className='flex flex-col md:flex-row justify-center items-center w-full gap-5'>
+					<Select
+						onValueChange={onExtensionChange}
+						defaultValue={fileExt}>
+						<SelectTrigger className='w-full md:w-[180px]'>
+							<SelectValue placeholder='File format' />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectItem value='svg'>SVG</SelectItem>
+							<SelectItem value='png'>PNG</SelectItem>
+							<SelectItem value='jpeg'>JPEG</SelectItem>
+							<SelectItem value='webp'>WEBP</SelectItem>
+						</SelectContent>
+					</Select>
+					<Button
+						onClick={onDownloadClick}
+						className='inline-flex items-center gap-2 place-content-center'>
+						Download <RxDownload />
+					</Button>
+				</div>
 			</div>
 		</div>
 	)
--- store/useOptionsStore.ts
+++ store/useOptionsStore.ts
@@ -1,74 +1,140 @@
-import { Options } from 'qr-code-styling'
-import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { Options } from "qr-code-styling"
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
 // Define your types
-export type Extension = 'svg' | 'png' | 'jpeg'
-export type DrawType = 'svg' | 'canvas'
+export type Extension = "svg" | "png" | "jpeg"
+export type DrawType = "svg" | "canvas"
 export type TypeNumber = number
-export type Mode = 'Numeric' | 'Alphanumeric' | 'Byte' | 'Kanji'
-export type ErrorCorrectionLevel = 'L' | 'M' | 'Q' | 'H'
-export type DotType = 'rounded' | 'dots' | 'classy' | string
-export type CornerSquareType = 'dot' | 'square' | 'extra-rounded'
-export type CornerDotType = 'dot' | 'square'
-
-
+export type Mode = "Numeric" | "Alphanumeric" | "Byte" | "Kanji"
+export type ErrorCorrectionLevel = "L" | "M" | "Q" | "H"
+export type DotType = "rounded" | "dots" | "classy" | string
+export type CornerSquareType = "dot" | "square" | "extra-rounded"
+export type CornerDotType = "dot" | "square"
 
 interface OptionsState {
-  options: Options
-  setOptions: (newOptions: Partial<Options>) => void
-  replaceOptions: (allOptions: Options) => void
-  fileExt: Extension
-  setFileExt: (ext: Extension) => void
+	options: Options
+	setOptions: (newOptions: Partial<Options>) => void
+	replaceOptions: (allOptions: Options) => void
+	fileExt: Extension
+	setFileExt: (ext: Extension) => void
+	setBackgroundColor: (color: string) => void
+	setDotsColor: (color: string) => void
+	setCornersColor: (color: string) => void
+	setCornersDotColor: (color: string) => void
+	resetColors: () => void
 }
 
 export const useOptionsStore = create<OptionsState>()(
-  persist(
-    (set) => ({
-      options: {
-        width: 300,
-        height: 300,
-        type: 'svg',
-        data: 'http://your-website.com',
-        image: '/favicon.ico',
-        margin: 10,
-        qrOptions: {
-          typeNumber: 0,
-          mode: 'Byte',
-          errorCorrectionLevel: 'Q',
-        },
-        imageOptions: {
-          hideBackgroundDots: true,
-          imageSize: 0.4,
-          margin: 20,
-          crossOrigin: 'anonymous',
-        },
-        dotsOptions: {
-          color: '#222222',
-          type: 'rounded',
-        },
-        backgroundOptions: {
-          color: '#5FD4F3',
-        },
-        cornersSquareOptions: {
-          color: '#222222',
-          type: 'extra-rounded',
-        },
-        cornersDotOptions: {
-          color: '#222222',
-          type: 'dot',
-        },
-      },
-      setOptions: (newOptions) =>
-        set((state) => ({
-          options: { ...state.options, ...newOptions },
-        })),
-      replaceOptions: (allOptions) => set({ options: allOptions }),
-      fileExt: 'svg',
-      setFileExt: (ext) => set({ fileExt: ext }),
-    }),
-    {
-      name: 'qr-options-storage',
-    }
-  )
+	persist(
+		(set) => ({
+			options: {
+				width: 300,
+				height: 300,
+				type: "svg",
+				data: "http://your-website.com",
+				// image: "/favicon.ico",
+				margin: 10,
+				qrOptions: {
+					typeNumber: 0,
+					mode: "Byte",
+					errorCorrectionLevel: "Q",
+				},
+				imageOptions: {
+					hideBackgroundDots: true,
+					imageSize: 0.4,
+					margin: 20,
+					crossOrigin: "anonymous",
+				},
+				dotsOptions: {
+					color: "#222222",
+					type: "rounded",
+				},
+				backgroundOptions: {
+					color: "#ffffff",
+				},
+				cornersSquareOptions: {
+					color: "#222222",
+					type: "extra-rounded",
+				},
+				cornersDotOptions: {
+					color: "#222222",
+					type: "dot",
+				},
+			},
+			setOptions: (newOptions) =>
+				set((state) => ({
+					options: { ...state.options, ...newOptions },
+				})),
+			replaceOptions: (allOptions) => set({ options: allOptions }),
+			fileExt: "svg",
+			setFileExt: (ext) => set({ fileExt: ext }),
+			setBackgroundColor: (color) =>
+				set((state) => ({
+					options: {
+						...state.options,
+						backgroundOptions: {
+							...state.options.backgroundOptions,
+							color: color,
+						},
+					},
+				})),
+			setDotsColor: (color) =>
+				set((state) => ({
+					options: {
+						...state.options,
+						dotsOptions: {
+							...state.options.dotsOptions,
+							color: color,
+						},
+					},
+				})),
+			setCornersColor: (color) =>
+				set((state) => ({
+					options: {
+						...state.options,
+						cornersSquareOptions: {
+							...state.options.cornersSquareOptions,
+							color: color,
+						},
+					},
+				})),
+			setCornersDotColor: (color) =>
+				set((state) => ({
+					options: {
+						...state.options,
+						cornersDotOptions: {
+							...state.options.cornersDotOptions,
+							color: color,
+						},
+					},
+				})),
+			resetColors: () =>
+				set((state) => ({
+					options: {
+						...state.options,
+						backgroundOptions: {
+							...state.options.backgroundOptions,
+							color: "#ffffff",
+						},
+						dotsOptions: {
+							...state.options.dotsOptions,
+							color: "#222222",
+						},
+						cornersSquareOptions: {
+							...state.options.cornersSquareOptions,
+							color: "#222222",
+						},
+						cornersDotOptions: {
+							...state.options.cornersDotOptions,
+							color: "#222222",
+						},
+					},
+				})),
+		}),
+
+		{
+			name: "qr-options-storage",
+		}
+	)
 )
